b10534f52346c6e20489135847da34bcff73e191
James Addison <james@reciperadar.com>
2019-05-31 23:31:18 +0000

    Email registration and verification endpoints

--- reciperadar/app.py
+++ reciperadar/app.py
@@ -1,8 +1,15 @@
+from base58 import b58encode
+from datetime import datetime
 from flask import Flask, jsonify, request
+from sqlalchemy.exc import IntegrityError
+from uuid import uuid4
+from validate_email import validate_email
 
 from reciperadar.course import Course
+from reciperadar.email import Email
 from reciperadar.ingredient import Ingredient
 from reciperadar.recipe import Recipe
+from reciperadar.services.database import Database
 
 app = Flask(__name__)
 
@@ -27,3 +34,36 @@ def recipes():
     exclude = request.args.getlist('exclude[]')
     results = Recipe().search(include, exclude)
     return jsonify(results)
+
+
+@app.route('/api/emails/register')
+def register_email():
+    email = request.args.get('email')
+    if not validate_email(email):
+        return jsonify({'error': 'invalid_email'}), 400
+
+    token = b58encode(uuid4().bytes).decode('utf-8')
+    record = Email(
+        email=email,
+        token=token
+    )
+
+    session = Database().get_session()
+    session.add(record)
+    try:
+        session.commit()
+    except IntegrityError:
+        pass
+    return jsonify({})
+
+
+@app.route('/api/emails/verify')
+def verify_email():
+    token = request.args.get('token')
+
+    session = Database().get_session()
+    email = session.query(Email).filter(Email.token == token).first()
+    if email:
+        email.verified_at = datetime.utcnow()
+        session.commit()
+    return jsonify({'token': token})
---
+++ reciperadar/email.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from sqlalchemy import Column, DateTime, String
+
+from reciperadar.base import Storable
+
+
+class Email(Storable):
+    __tablename__ = 'emails'
+
+    email = Column(String, primary_key=True)
+    token = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    verified_at = Column(DateTime)
