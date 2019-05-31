e985c9ce3bef63f0c65d5e90f2f123d854c7c5d4
James Addison <james@reciperadar.com>
2019-05-31 13:02:51 +0100

    Add basic reminder test

---
+++ tests/test_reminders.py
@@ -0,0 +1,22 @@
+import pytest
+
+from datetime import datetime, timedelta
+
+from reciperadar.reminders import MealReminder
+
+
+@pytest.fixture
+def future_meal_args():
+    today_dt = datetime.combine(datetime.today(), datetime.min.time())
+    future_meal_dt = today_dt + timedelta(weeks=1, hours=18)
+    future_meal_dur = timedelta(hours=1, minutes=30)
+
+    return {
+        'title': 'Test Meal',
+        'start_time': future_meal_dt,
+        'duration': future_meal_dur,
+    }
+
+
+def test_construct_mealreminder(future_meal_args):
+    MealReminder(**future_meal_args)
