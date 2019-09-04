1642ca560457f535b2aefbe7f0b1fea10b69d48b
James Addison <james@reciperadar.com>
2019-09-04 17:38:00 +0000

    Record search and redirect events

--- reciperadar/api/redirect.py
+++ reciperadar/api/redirect.py
@@ -2,7 +2,9 @@ from flask import abort, redirect
 
 from reciperadar import app
 from reciperadar.models.recipe import Recipe
+from reciperadar.models.events.redirect import RedirectEvent
 from reciperadar.services.database import Database
+from reciperadar.workers.events import store_event
 
 
 @app.route('/api/redirect/recipe/<recipe_id>')
@@ -12,4 +14,11 @@ def recipe_redirect(recipe_id):
     if not recipe:
         return abort(404)
     session.close()
+
+    store_event(RedirectEvent(
+        recipe_id=recipe.id,
+        domain=recipe.domain,
+        src=recipe.src
+    ))
+
     return redirect(recipe.src, code=301)
--- reciperadar/api/search.py
+++ reciperadar/api/search.py
@@ -3,6 +3,8 @@ from flask import jsonify, request
 from reciperadar import app
 from reciperadar.models.ingredient import Ingredient
 from reciperadar.models.recipe import Recipe
+from reciperadar.models.events.search import SearchEvent
+from reciperadar.workers.events import store_event
 
 
 @app.route('/api/ingredients')
@@ -20,4 +22,15 @@ def recipes():
     limit = min(request.args.get('limit', type=int, default=10), 10)
     sort = request.args.get('sort')
     results = Recipe().search(include, exclude, offset, limit, sort)
+
+    store_event(SearchEvent(
+        include=include,
+        exclude=exclude,
+        offset=offset,
+        limit=limit,
+        sort=sort,
+        results_ids=[result['id'] for result in results['results']],
+        results_total=results['total']
+    ))
+
     return jsonify(results)
