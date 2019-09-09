92118c829e4d08488b60737ae740e4963ddcfbe7
James Addison <james@reciperadar.com>
2019-09-09 12:37:19 +0100

    Create and populate recipe_urls.domain field

--- reciperadar/models/url.py
+++ reciperadar/models/url.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, DateTime, Integer, String
+import tldextract
 
 from reciperadar.models.base import Storable
 
@@ -6,6 +7,14 @@ from reciperadar.models.base import Storable
 class RecipeURL(Storable):
     __tablename__ = 'recipe_urls'
 
+    def __init__(self, *args, **kwargs):
+        if 'url' in kwargs:
+            url_info = tldextract.extract(kwargs['url'])
+            domain = f'{url_info.domain}.{url_info.suffix}'
+            kwargs['domain'] = domain
+        super().__init__(*args, **kwargs)
+
     url = Column(String, primary_key=True)
+    domain = Column(String)
     crawled_at = Column(DateTime)
     crawl_status = Column(Integer)
