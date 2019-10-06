625026ac9719889fcb3f0e501b6a1ecc0424f277
James Addison <james@reciperadar.com>
2019-10-06 12:17:27 -0700

    Allow crawling and indexing from private IP addresses

--- reciperadar/api/recipes.py
+++ reciperadar/api/recipes.py
@@ -1,3 +1,5 @@
+from ipaddress import ip_address
+
 from flask import abort, jsonify, request
 
 from reciperadar import app
@@ -7,7 +9,7 @@ from reciperadar.workers.recipes import crawl_recipe, index_recipe
 @app.route('/api/recipes/crawl', methods=['POST'])
 def recipe_crawl():
     forwarded_for = request.headers.get('x-forwarded-for')
-    if forwarded_for:
+    if forwarded_for and not ip_address(forwarded_for).is_private:
         return abort(403)
 
     url = request.form.get('url')
@@ -21,7 +23,7 @@ def recipe_crawl():
 @app.route('/api/recipes/index', methods=['POST'])
 def recipe_index():
     forwarded_for = request.headers.get('x-forwarded-for')
-    if forwarded_for:
+    if forwarded_for and not ip_address(forwarded_for).is_private:
         return abort(403)
 
     recipe_id = request.form.get('recipe_id')
