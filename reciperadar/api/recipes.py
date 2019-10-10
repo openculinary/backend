493de623efecc8f787ae73f8c6008a9b99e924e2
James Addison <james@reciperadar.com>
2019-10-10 11:06:10 +0000

    Add internal endpoint to return recipe model details

--- reciperadar/api/recipes.py
+++ reciperadar/api/recipes.py
@@ -3,7 +3,27 @@ from ipaddress import ip_address
 from flask import abort, jsonify, request
 
 from reciperadar import app
+from reciperadar.models.recipes import Recipe
+from reciperadar.services.database import Database
 from reciperadar.workers.recipes import crawl_recipe, index_recipe
+
+
+@app.route('/api/recipes/<recipe_id>')
+def recipe_get(recipe_id):
+    forwarded_for = request.headers.get('x-forwarded-for')
+    if forwarded_for and not ip_address(forwarded_for).is_private:
+        return abort(403)
+
+    session = Database().get_session()
+    recipe = session.query(Recipe).get(recipe_id)
+    if not recipe:
+        session.close()
+        return abort(404)
+
+    response = recipe.to_doc()
+    session.close()
+
+    return jsonify(response)
 
 
 @app.route('/api/recipes/crawl', methods=['POST'])
