4b505cae920704bf48bd9159bb678fa1278247cf
James Addison <james@reciperadar.com>
2019-10-24 09:37:16 -0700

    Cleanup and consistency

--- build.sh
+++ build.sh
@@ -1,19 +1,9 @@
-REGISTRY='registry.gitlab.com'
-PROJECT='openculinary'
+REGISTRY='registry.openculinary.org'
+PROJECT='reciperadar'
+SERVICE=$(basename `git rev-parse --show-toplevel`)
 
-IMAGE_NAME=${REGISTRY}/${PROJECT}/$(basename `git rev-parse --show-toplevel`)
+IMAGE_NAME=${REGISTRY}/${PROJECT}/${SERVICE}
 IMAGE_COMMIT=$(git rev-parse --short HEAD)
-
-if [ -n "${GITLAB_USER_ID}" ]; then
-    # Override the default 'overlay' storage driver, which fails GitLab builds
-    export STORAGE_DRIVER='vfs'
-
-    # Workaround from https://major.io for 'overlay.mountopt' option conflict
-    sed -i '/^mountopt =.*/d' /etc/containers/storage.conf
-
-    # Authenticate for any privileged operations
-    REGISTRY_AUTH_FILE=${HOME}/auth.json echo "${CI_REGISTRY_PASSWORD}" | buildah login -u "${CI_REGISTRY_USER}" --password-stdin ${CI_REGISTRY}
-fi
 
 container=$(buildah from docker.io/library/python:3.7-alpine)
 buildah copy ${container} 'reciperadar' 'reciperadar'
@@ -22,7 +12,3 @@ buildah run ${container} -- pip install pipenv
 buildah run ${container} -- pipenv install
 buildah config --port 80 --entrypoint 'pipenv run gunicorn reciperadar:app --bind :80' ${container}
 buildah commit --squash --rm ${container} ${IMAGE_NAME}:${IMAGE_COMMIT}
-
-if [ -n "${GITLAB_USER_ID}" ]; then
-    buildah push ${IMAGE_NAME}:${IMAGE_COMMIT}
-fi
