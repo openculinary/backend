8d9bb067f996416a00e301ea394ec50d4672e62e
James Addison <james@reciperadar.com>
2019-10-31 06:25:35 -0700

    Use X-RecipeRadar-External header

--- reciperadar/utils/decorators.py
+++ reciperadar/utils/decorators.py
@@ -1,14 +1,13 @@
 from functools import wraps
 
 from flask import abort, request
-from ipaddress import ip_address
 
 
 def internal(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        forwarded_for = request.headers.get('x-forwarded-for')
-        if forwarded_for and not ip_address(forwarded_for).is_private:
+        external = request.headers.get('x-reciperadar-external')
+        if external:
             return abort(403)
         return f(*args, **kwargs)
     return decorated
