bdb85338f17ef9ae2d7ff536a78bda17ab27f9f5
James Addison <james@reciperadar.com>
2019-11-25 18:45:48 +0000

    Improve test readability

--- tests/models/test_url.py
+++ tests/models/test_url.py
@@ -5,38 +5,34 @@ from reciperadar.models.url import CrawlURL, RecipeURL
 
 
 @pytest.fixture
-def crawl_url():
+def origin_url():
     return 'https://recipe.subdomain.example.com/recipe/123'
 
 
 @pytest.fixture
-def crawl_headers(recipe_url):
-    return {'Location': recipe_url}
+def content_url(origin_url):
+    return origin_url.replace('subdomain', 'migrated')
 
 
-@pytest.fixture
-def recipe_url(crawl_url):
-    return crawl_url.replace('subdomain', 'migrated')
-
-
-def test_crawl_url_domain(crawl_url):
-    url = CrawlURL(url=crawl_url)
+def test_origin_url_domain(origin_url):
+    url = CrawlURL(url=origin_url)
 
     assert url.domain == 'example.com'
 
 
 @responses.activate
-def test_crawl_url_resolution(crawl_url, crawl_headers, recipe_url):
-    responses.add(responses.GET, crawl_url, status=301, headers=crawl_headers)
-    responses.add(responses.GET, recipe_url, status=200)
+def test_origin_url_resolution(origin_url, content_url):
+    redir_headers = {'Location': content_url}
+    responses.add(responses.GET, origin_url, status=301, headers=redir_headers)
+    responses.add(responses.GET, content_url, status=200)
 
-    crawl_input = CrawlURL(url=crawl_url)
-    crawl_output = crawl_input.crawl()
+    crawl_url = CrawlURL(url=origin_url)
+    recipe_url = crawl_url.crawl()
 
-    assert (crawl_input.url, crawl_output.url) == (crawl_url, recipe_url)
+    assert (crawl_url.url, recipe_url.url) == (origin_url, content_url)
 
 
-def test_recipe_url_domain(recipe_url):
-    url = RecipeURL(url=recipe_url)
+def test_content_url_domain(content_url):
+    url = RecipeURL(url=content_url)
 
     assert url.domain == 'example.com'
