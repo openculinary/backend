7e196763a7e6edeed08fd4452b16a4f5657bf97b
James Addison <james@reciperadar.com>
2020-05-27 19:07:25 +0100

    Remove CORS functionality from backend

--- reciperadar/__init__.py
+++ reciperadar/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask_cors import CORS
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 from reciperadar.services.database import DB_URI
@@ -12,11 +11,6 @@ app.config.update(
 )
 app.url_map.strict_slashes = False
 app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
-CORS(app, origins=[
-    r'^https://\w+.reciperadar.com$',
-    r'^http://localhost$',
-    r'^http://192.168.\d+.\d+$',
-])
 
 
 import reciperadar.api.products
