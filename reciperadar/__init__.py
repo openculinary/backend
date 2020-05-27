0255ca72736073d87fcf4cb50030fdedd07f8d7a
James Addison <james@reciperadar.com>
2020-05-27 19:01:43 +0100

    Remove user-facing feedback functionality from backend

--- reciperadar/__init__.py
+++ reciperadar/__init__.py
@@ -1,8 +1,5 @@
-import os
-
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
-from flask_mail import Mail
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 from reciperadar.services.database import DB_URI
@@ -10,11 +7,6 @@ from reciperadar.services.database import DB_URI
 
 app = Flask(__name__)
 app.config.update(
-    MAIL_SERVER='smtp.gmail.com',
-    MAIL_PORT=587,
-    MAIL_USE_TLS=True,
-    MAIL_USERNAME=os.environ.get('MAIL_USERNAME'),
-    MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD'),
     SQLALCHEMY_DATABASE_URI=DB_URI,
     SQLALCHEMY_TRACK_MODIFICATIONS=False,
 )
@@ -25,10 +17,8 @@ CORS(app, origins=[
     r'^http://localhost$',
     r'^http://192.168.\d+.\d+$',
 ])
-mail = Mail(app)
 
 
-import reciperadar.api.feedback
 import reciperadar.api.products
 import reciperadar.api.recipes
 import reciperadar.api.redirect
