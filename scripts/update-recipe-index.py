5c59080eda8cba83397731e2cbd89ac489d3d4c9
James Addison <james@reciperadar.com>
2020-06-22 23:08:04 +0100

    Autosuggest: generate edge ngrams over ingredient names (#12)

--- scripts/update-recipe-index.py
+++ scripts/update-recipe-index.py
@@ -2,6 +2,38 @@ import argparse
 from elasticsearch import Elasticsearch
 import sys
 
+settings = {
+    'index': {
+        'number_of_replicas': 0,
+        'refresh_interval': '300s',
+    },
+    'analysis': {
+        'analyzer': {
+            'autocomplete_analyze': {
+                'tokenizer': 'autocomplete_tokenize',
+                'filter': ['lowercase']
+            },
+            'autocomplete_search': {
+                'tokenizer': 'standard',
+                'filter': ['lowercase', 'autocomplete_filter']
+            }
+        },
+        'filter': {
+            'autocomplete_filter': {
+                'type': 'truncate',
+                'length': 10
+            }
+        },
+        'tokenizer': {
+            'autocomplete_tokenize': {
+                'type': 'edge_ngram',
+                'min_gram': 3,
+                'max_gram': 10,
+                'token_chars': ['letter']
+            }
+        }
+    }
+}
 mapping = {
     'properties': {
         'directions': {
@@ -29,7 +61,16 @@ mapping = {
                 'product': {
                     'properties': {
                         'product_id': {'type': 'keyword'},
-                        'product': {'type': 'text'},
+                        'product': {
+                            'type': 'keyword',
+                            'fields': {
+                                'autocomplete': {
+                                    'type': 'text',
+                                    'analyzer': 'autocomplete_analyze',
+                                    'search_analyzer': 'autocomplete_search'
+                                }
+                            }
+                        },
                         'category': {'type': 'keyword'},
                         'is_plural': {'type': 'boolean'},
                         'singular': {'type': 'keyword'},
@@ -41,12 +82,6 @@ mapping = {
         'contents': {'type': 'keyword'}
     }
 }
-settings = {
-    'index': {
-        'number_of_replicas': 0,
-        'refresh_interval': '300s',
-    }
-}
 
 parser = argparse.ArgumentParser(description='Configure recipes search index')
 parser.add_argument('--hostname', required=True)
@@ -55,8 +90,10 @@ args = parser.parse_args()
 
 es = Elasticsearch(args.hostname)
 try:
-    es.indices.put_mapping(index=args.index, body=mapping)
+    es.indices.close(index=args.index)
     es.indices.put_settings(index=args.index, body=settings)
+    es.indices.put_mapping(index=args.index, body=mapping)
+    es.indices.open(index=args.index)
 except Exception as e:
     print('Failed to create recipe index: {}'.format(e))
     sys.exit(1)
