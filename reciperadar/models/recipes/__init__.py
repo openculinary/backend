86a07e8e4f936fb355c4baa3c85186e6380513b2
James Addison <james@reciperadar.com>
2020-06-26 16:22:38 +0100

    Cleanup: remove autosuggest-related code

--- reciperadar/models/recipes/__init__.py
+++ reciperadar/models/recipes/__init__.py
@@ -1,5 +1,4 @@
 from .recipe import Recipe
 from .direction import RecipeDirection
-from .equipment import RecipeEquipment
 from .ingredient import RecipeIngredient
 from .product import IngredientProduct
