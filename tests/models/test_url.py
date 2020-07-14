1278ea3cefbb72e0b71b523ef0f440e998c6cd1c
James Addison <james@reciperadar.com>
2020-07-14 19:19:12 +0100

    Add initial crawl URL redirection timeline test case

--- tests/models/test_url.py
+++ tests/models/test_url.py
@@ -2,6 +2,8 @@ import pytest
 import requests
 from unittest.mock import patch
 
+from datetime import datetime
+
 from reciperadar.models.url import CrawlURL, RecipeURL
 
 
@@ -38,12 +40,24 @@ def test_content_url_domain(content_url):
     assert url.domain == 'example.com'
 
 
-def test_crawl_url_session(db_session, origin_url):
-    url = CrawlURL(url=origin_url)
-    db_session.add(url)
-    db_session.commit()
-    assert CrawlURL.query.count() == 1
+def test_crawl_url_timeline(db_session):
+    path = [
+        (datetime(2020, 1, 1), 'A', 'B'),
+        (datetime(2020, 2, 1), 'B', 'C'),
+        (datetime(2020, 3, 1), 'C', 'D'),
+        (datetime(2020, 4, 1), 'D', 'D'),
+    ]
+    path = [
+        CrawlURL(url=from_url, resolves_to=to_url, crawled_at=time)
+        for (time, from_url, to_url) in path
+    ]
+    for step in path:
+        db_session.add(step)
 
+    recipe = RecipeURL(url='C')
+    earliest_crawl = recipe.find_earliest_crawl()
+    latest_crawl = recipe.find_latest_crawl()
 
-def test_crawl_url_session_reset(db_session):
-    assert CrawlURL.query.count() == 0
+    assert earliest_crawl.url == 'A'
+    assert latest_crawl.url == 'D'
+    assert latest_crawl.resolves_to == 'D'
