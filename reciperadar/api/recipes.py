861b52dcad37a5557a5fdc6966f2790dec39181e
James Addison <james@reciperadar.com>
2020-07-15 17:13:53 +0100

    Rename response field: current_crawl -> latest_crawl

--- reciperadar/api/recipes.py
+++ reciperadar/api/recipes.py
@@ -25,7 +25,7 @@ def recipe_diagnostics(recipe_id):
     return jsonify({
         'id': recipe.id,
         'indexed_at': recipe.indexed_at,
-        'current_crawl': {
+        'latest_crawl': {
             'url': recipe_url.url,
             'crawled_at': recipe_url.crawled_at,
             'crawl_status': recipe_url.crawl_status,
