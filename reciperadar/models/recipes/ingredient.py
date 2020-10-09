465f535bafb28dbf418fc06980ee8ea4cc113c6a
James Addison <james@reciperadar.com>
2020-10-09 16:24:33 +0100

    Add IngredientNutrition database model

--- reciperadar/models/recipes/ingredient.py
+++ reciperadar/models/recipes/ingredient.py
@@ -1,6 +1,7 @@
 from reciperadar import db
 from reciperadar.models.base import Searchable, Storable
 from reciperadar.models.recipes.product import IngredientProduct
+from reciperadar.models.recipes.nutrition import IngredientNutrition
 
 
 class RecipeIngredient(Storable, Searchable):
@@ -15,6 +16,12 @@ class RecipeIngredient(Storable, Searchable):
     markup = db.Column(db.String)
     product = db.relationship(
         'IngredientProduct',
+        backref='recipe_ingredient',
+        uselist=False,
+        passive_deletes='all'
+    )
+    nutrition = db.relationship(
+        'IngredientNutrition',
         backref='recipe_ingredient',
         uselist=False,
         passive_deletes='all'
@@ -35,6 +42,8 @@ class RecipeIngredient(Storable, Searchable):
             description=doc['description'].strip(),
             markup=doc.get('markup'),
             product=IngredientProduct.from_doc(doc['product']),
+            nutrition=IngredientNutrition.from_doc(doc['nutrition'])
+            if 'nutrition' in doc else None,
             magnitude=doc.get('magnitude') or doc.get('quantity'),  # TODO
             magnitude_parser=(
                 doc.get('magnitude_parser') or doc.get('quantity_parser')
