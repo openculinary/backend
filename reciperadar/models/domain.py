b7cfc035be66a1cdc6f2234fd4cd4f65df01e880
James Addison <james@reciperadar.com>
2020-10-16 11:27:49 +0100

    Import db module

--- reciperadar/models/domain.py
+++ reciperadar/models/domain.py
@@ -1,8 +1,9 @@
+from reciperadar import db
 from reciperadar.models.base import Searchable, Storable
 
 
 class Domain(Searchable, Storable):
     __tablename__ = 'domains'
 
-    domain = Column(String, primary_key=True)
-    image_src = Column(String)
+    domain = db.Column(db.String, primary_key=True)
+    image_src = db.Column(db.String)
