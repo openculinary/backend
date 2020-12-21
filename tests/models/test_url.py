352842a746029c29a0bb3b2f54e11d059fb8546f
James Addison <james@reciperadar.com>
2020-12-21 13:54:31 +0000

    Migrate to the 'tld' Python library

--- tests/models/test_url.py
+++ tests/models/test_url.py
@@ -50,16 +50,20 @@ def test_crawl_url_timeline(db_session):
         (datetime(2020, 4, 1), 'D', 'D'),
     ]
     path = [
-        CrawlURL(url=from_url, resolves_to=to_url, crawled_at=time)
-        for (time, from_url, to_url) in path
+        CrawlURL(
+            url=f'//example.org/{from_node}',
+            resolves_to=f'//example.org/{to_node}',
+            crawled_at=time
+        )
+        for (time, from_node, to_node) in path
     ]
     for step in path:
         db_session.add(step)
 
-    recipe = RecipeURL(url='C')
+    recipe = RecipeURL(url='//example.org/C')
     earliest_crawl = recipe.find_earliest_crawl()
     latest_crawl = recipe.find_latest_crawl()
 
-    assert earliest_crawl.url == 'A'
-    assert latest_crawl.url == 'D'
-    assert latest_crawl.resolves_to == 'D'
+    assert earliest_crawl.url == '//example.org/A'
+    assert latest_crawl.url == '//example.org/D'
+    assert latest_crawl.resolves_to == '//example.org/D'
