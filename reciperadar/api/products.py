b99a5fd8dc59ea0dfdf370891677d60d7bbe2af3
James Addison <james@reciperadar.com>
2022-07-20 19:24:03 +0100

    Stream all names for each product in product export hierarchy

--- reciperadar/api/products.py
+++ reciperadar/api/products.py
@@ -15,28 +15,18 @@ def stream(items):
         yield f"{line}\n"
 
 
-def _product_map(products):
-    product_map = {}
+def _product_stream(products):
     for product, name, nutrition, count, plural_count in products.all():
-        product.name = name
-        product.nutrition = nutrition
-        product.count = count or 0
-        product.plural_count = plural_count or 0
-        product_map[product.id] = product
-    return product_map
-
-
-def _product_stream(product_map):
-    for product in product_map.values():
-        is_plural = product.plural_count > product.count - product.plural_count
+        plural_count = plural_count or 0
+        is_plural = plural_count > count - plural_count
         result = {
-            "product": product.name.plural if is_plural else product.name.singular,
-            "recipe_count": product.count,
+            "product": name.plural if is_plural else name.singular,
+            "recipe_count": count,
             "id": product.id,
             "parent_id": product.parent_id,
         }
-        if product.nutrition:
-            result["nutrition"] = product.nutrition.to_doc()
+        if nutrition:
+            result["nutrition"] = nutrition.to_doc()
         yield result
 
 
@@ -60,6 +50,5 @@ def hierarchy():
         )
     )
 
-    product_map = _product_map(products)
-    products = _product_stream(product_map)
+    products = _product_stream(products)
     return Response(stream(products), content_type="application/x-ndjson")
