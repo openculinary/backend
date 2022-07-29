e0bd88dac2320728a929dd4b55f6b5b55afd35be
James Addison <james@reciperadar.com>
2022-07-29 19:15:29 +0100

    Remove TODO note related to sqlalchemy relation population
    This seems to be an expected behaviour of the library

--- tests/models/recipes/test_recipe.py
+++ tests/models/recipes/test_recipe.py
@@ -5,7 +5,6 @@ def test_recipe_from_doc(db_session, raw_recipe_hit):
     recipe = Recipe().from_doc(raw_recipe_hit["_source"])
 
     # Commit recipe to the session; performs db object relationship resolution
-    # TODO: find a more robust way to ensure that related objects are loaded
     db_session.add(recipe)
     db_session.commit()
 
@@ -58,7 +57,6 @@ def test_hidden_recipe(db_session, raw_recipe_hit):
     recipe = Recipe().from_doc(raw_recipe_hit["_source"])
 
     # Commit recipe to the session; performs db object relationship resolution
-    # TODO: find a more robust way to ensure that related objects are loaded
     db_session.add(recipe)
     db_session.commit()
 
@@ -72,7 +70,6 @@ def test_nutritional_filtering(db_session, raw_recipe_hit):
     recipe = Recipe().from_doc(raw_recipe_hit["_source"])
 
     # Commit recipe to the session; performs db object relationship resolution
-    # TODO: find a more robust way to ensure that related objects are loaded
     db_session.add(recipe)
     db_session.commit()
 
