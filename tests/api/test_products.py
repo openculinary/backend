86901d79a5bc8765930be3896e8a30f2245c0547
James Addison <james@reciperadar.com>
2023-06-15 13:02:28 +0100

    Test coverage: add assertions on the content of the product hierarchy endpoint response

--- tests/api/test_products.py
+++ tests/api/test_products.py
@@ -1,4 +1,13 @@
-def test_retrieve_product_hierarchy(client):
+import json
+
+
+def test_retrieve_product_hierarchy(client, products):
+    expected_product_ids = {"one", "two", "ancestor_of_one"}
+
     response = client.get(path="/products/hierarchy")
+    products = response.data.decode("utf-8").splitlines()
+    product_ids = {json.loads(product)["id"] for product in products}
 
     assert response.status_code == 200
+    assert len(products) == 3
+    assert product_ids == expected_product_ids
