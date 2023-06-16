a951d98fb8563ef1123c5d3f78be6e4728f56ced
James Addison <james@reciperadar.com>
2023-06-16 10:56:57 +0100

    Adjust/improve test coverage: it should continue to fail without the bugfix applied so that regressions will be caught

--- tests/api/test_products.py
+++ tests/api/test_products.py
@@ -1,8 +1,31 @@
+from flask import current_app
 import json
+from unittest.mock import patch
+
+from reciperadar.models.recipes.product import ProductName
 
 
-def test_retrieve_product_hierarchy(client, products):
-    expected_product_ids = {"one", "two", "ancestor_of_one"}
+def mock_query():
+    """
+    Generator method that lazily performs a flask application context lookup, to
+    replicate bug https://github.com/openculinary/backend/issues/65 and then yields
+    mock data.
+    """
+    current_app._get_current_object()
+    yield from [
+        # name, nutrition, count, plural_count
+        (ProductName(id="butter"), None, 1, 1),
+        (ProductName(id="orange"), None, 5, 2),
+        (ProductName(id="apple"), None, 5, 3),
+    ]
+
+
+@patch("reciperadar.api.products.db.session.query")
+def test_retrieve_product_hierarchy(query, client):
+    (
+        query.return_value.join.return_value.join.return_value.group_by.return_value
+    ).all.return_value = mock_query()
+    expected_product_ids = {"apple", "butter", "orange"}
 
     response = client.get(path="/products/hierarchy")
     products = response.data.decode("utf-8").splitlines()
