f8565ec71a7e2abe1f1570172e8ee7d1fe9f768c
James Addison <55152140+jayaddison@users.noreply.github.com>
2023-08-28 12:39:49 +0100

    Configure 'events' schema for all event-log-related tables (#76)

--- reciperadar/models/events/base.py
+++ reciperadar/models/events/base.py
@@ -9,6 +9,7 @@ from reciperadar.models.base import Storable
 class BaseEvent(Storable):
     __abstract__ = True
     __metaclass__ = ABC
+    __table_args__ = {"schema": "events"}
 
     event_id = db.Column(db.String, default=uuid4, primary_key=True)
     logged_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
