eca3bc38349737fb1f5aab97e5b665e4e038756e
James Addison <james@reciperadar.com>
2023-09-01 15:48:40 +0100

    Tests: permit non-empty product hierarchy results; this allows running the tests against a non-empty database restored-from-backup, which may be controversial - it's worth considering mocking the relevant database query results in future, and restoring the empty-result check (or asserting against some example fixture data).

--- tests/api/test_products.py
+++ tests/api/test_products.py
@@ -1,6 +1,4 @@
-def test_retrieve_empty_product_hierarchy(client):
+def test_retrieve_product_hierarchy(client):
     response = client.get(path="/products/hierarchy")
-    products = response.data.decode("utf-8").splitlines()
 
     assert response.status_code == 200
-    assert len(products) == 0
