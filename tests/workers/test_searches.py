ecbbb1f822b839d538ad0948474d41387ebb3d8a
James Addison <james@reciperadar.com>
2024-03-28 19:04:45 +0000

    Fixup: relocate pytest.mark.respx decorators from fixtures to test functions.

--- tests/workers/test_searches.py
+++ tests/workers/test_searches.py
@@ -3,17 +3,16 @@ from reciperadar.workers.searches import recrawl_search
 
 
 @pytest.fixture
-@pytest.mark.respx(base_url="http://recrawler-service")
 def error_response(respx_mock):
     respx_mock.post("/").respond(status_code=400)
 
 
 @pytest.fixture
-@pytest.mark.respx(base_url="http://recrawler-service")
 def valid_response(respx_mock):
     respx_mock.post("/").respond(json=["http://www.example.com"])
 
 
+@pytest.mark.respx(base_url="http://recrawler-service")
 def test_recrawler_error_handled(error_response):
     result = recrawl_search(
         include=["tofu"],
@@ -26,6 +25,7 @@ def test_recrawler_error_handled(error_response):
     assert result == []
 
 
+@pytest.mark.respx(base_url="http://recrawler-service")
 def test_recrawler_success(valid_response):
     result = recrawl_search(
         include=["tofu"],
