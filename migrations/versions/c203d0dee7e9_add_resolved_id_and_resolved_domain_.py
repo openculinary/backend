a44ee7e408fbc7ae9f054aaeb9ab3a2b450ea2bb
James Addison <james@reciperadar.com>
2025-01-28 16:32:57 +0000

    CrawlURL: update migration script to populate `resolved_id` and `resolved_domain` for existing records

--- migrations/versions/c203d0dee7e9_add_resolved_id_and_resolved_domain_.py
+++ migrations/versions/c203d0dee7e9_add_resolved_id_and_resolved_domain_.py
@@ -5,8 +5,13 @@ Revises: 11742ee5cedb
 Create Date: 2025-01-28 16:17:33.743557
 
 """
+from urllib.parse import urlparse
+
 from alembic import op
+from pymmh3 import hash_bytes
 import sqlalchemy as sa
+
+from reciperadar.models.url import BaseURL
 
 
 # revision identifiers, used by Alembic.
@@ -24,6 +29,18 @@ def upgrade():
         batch_op.create_index(batch_op.f('ix_crawl_urls_resolved_id'), ['resolved_id'], unique=False)
 
     # ### end Alembic commands ###
+
+    connection = op.get_bind()
+
+    records = connection.execute(sa.text('SELECT url, resolves_to FROM crawl_urls WHERE resolves_to IS NOT NULL'))
+    for (url, resolves_to) in records.fetchall():
+        resolved_url_hash = hash_bytes(resolves_to).encode("utf-8")
+        resolved_id = BaseURL.generate_id(resolved_url_hash)
+        resolved_domain = urlparse(resolves_to).netloc
+        connection.execute(
+            sa.text('UPDATE crawl_urls SET resolved_id = :resolved_id, resolved_domain = :resolved_domain WHERE url = :url'),
+            parameters={"resolved_id": resolved_id, "resolved_domain": resolved_domain, "url": url},
+        )
 
 
 def downgrade():
